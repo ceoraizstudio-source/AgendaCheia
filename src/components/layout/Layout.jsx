d6f7272c2b4ee2f2a1ec8c758d6ea6e7c7035e3c
ceoraizstudio-source <ceo.raiz.studio@gmail.com>
2026-05-01 11:33:49 -0300

    remove: topbar (photo, bell, settings icons)

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
-import Topbar from './Topbar'
 import NewLeadModal from '../leads/NewLeadModal'
 
 export default function Layout() {
@@ -8,7 +7,6 @@ export default function Layout() {
     <div className="flex min-h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
-        <Topbar />
         <main className="flex-1 px-8 py-8" style={{ minWidth: 0 }}>
           <Outlet />
         </main>
